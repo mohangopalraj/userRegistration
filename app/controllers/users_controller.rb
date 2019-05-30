16bd34d0b7de7daa2509fa0f254f206b332a2563
mohan <mohan@thezudo.com>
2019-05-30 21:50:31 +0530

    adding gem files and route configuration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ActionController::Base
+
+	def new
+	end
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			session[:user_id] = user.id
+			redirect_to '/'
+		else
+			redirect_to '/signup'
+		end
+	end
+
+end
